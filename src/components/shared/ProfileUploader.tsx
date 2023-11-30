8de4c71b639f2d0672678558f185872fc5be3606
omarghandour <alex.forget50@gmail.com>
2023-11-30 06:35:34 +0200

    first commit

--- src/components/shared/ProfileUploader.tsx
+++ src/components/shared/ProfileUploader.tsx
@@ -1,84 +1,49 @@
-import { Models } from "appwrite";
-import { Link } from "react-router-dom";
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
 
-import { multiFormatDateString } from "@/lib/utils";
-import { useUserContext } from "@/context/AuthContext";
-import PostStats from "./PostStats";
+import { convertFileToUrl } from "@/lib/utils";
 
-type PostCardProps = {
-  post: Models.Document;
+type ProfileUploaderProps = {
+  fieldChange: (files: File[]) => void;
+  mediaUrl: string;
 };
 
-const PostCard = ({ post }: PostCardProps) => {
-  const { user } = useUserContext();
+const ProfileUploader = ({ fieldChange, mediaUrl }: ProfileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
 
-  if (!post.creator) return;
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(convertFileToUrl(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpeg", ".jpg"],
+    },
+  });
 
   return (
-    <div className="post-card">
-      <div className="flex-between">
-        <div className="flex items-center gap-3">
-          <Link to={`/profile/${post.creator.$id}`}>
-            <img
-              src={
-                post.creator?.imageUrl ||
-                "/assets/icons/profile-placeholder.svg"
-              }
-              alt="creator"
-              className="w-12 lg:h-12 rounded-full"
-            />
-          </Link>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} className="cursor-pointer" />
 
-          <div className="flex flex-col">
-            <p className="base-medium lg:body-bold text-light-1">
-              {post.creator.name}
-            </p>
-            <div className="flex-center gap-2 text-light-3">
-              <p className="subtle-semibold lg:small-regular ">
-                {multiFormatDateString(post.$createdAt)}
-              </p>
-              •
-              <p className="subtle-semibold lg:small-regular">
-                {post.location}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <Link
-          to={`/update-post/${post.$id}`}
-          className={`${user.id !== post.creator.$id && "hidden"}`}>
-          <img
-            src={"/assets/icons/edit.svg"}
-            alt="edit"
-            width={20}
-            height={20}
-          />
-        </Link>
-      </div>
-
-      <Link to={`/posts/${post.$id}`}>
-        <div className="small-medium lg:base-medium py-5">
-          <p>{post.caption}</p>
-          <ul className="flex gap-1 mt-2">
-            {post.tags.map((tag: string, index: string) => (
-              <li key={`${tag}${index}`} className="text-light-3 small-regular">
-                #{tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-
+      <div className="cursor-pointer flex-center gap-4">
         <img
-          src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
-          alt="post image"
-          className="post-card_img"
+          src={fileUrl || "/assets/icons/profile-placeholder.svg"}
+          alt="image"
+          className="h-24 w-24 rounded-full object-cover object-top"
         />
-      </Link>
-
-      <PostStats post={post} userId={user.id} />
+        <p className="text-primary-500 small-regular md:bbase-semibold">
+          Change profile photo
+        </p>
+      </div>
     </div>
   );
 };
 
-export default PostCard;
+export default ProfileUploader;
